b9d8a22777e52181b73b8b78c3d20ffcd60739ce
abigael mwelu <abigael.mutuku@student.moringaschool.com>
2023-06-15 12:58:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1=31
+const num2=2
+let multiply= num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random()*10)
+
+const num3=24;
+const num4=6;
+let mod=num3 / num4
+let max=Math.max(2, 5, 10, 20)
